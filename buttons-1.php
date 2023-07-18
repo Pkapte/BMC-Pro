8cd36e8977e585edd79d3f0a7c5ec58c173b90ca
Pkapte <priya_bhimraojikapte_tp@bmc.com>
2023-07-18 18:27:46 +0530

    Added New Features

--- buttons-1.php
+++ buttons-1.php
@@ -10,6 +10,12 @@
             target="_self">
             See how Michigan leverages automation (2:16)
         </a>
+        <a 
+            class="btn video-play btn-secondary modal-youtube-video-player btn-play-icon"
+            href="/content/bmc/videos.html?vID=pG8wb2ig2Z8" 
+            target="_self">
+            See how Michigan leverages automation (2:16)
+        </a>
     </div>
     
 
