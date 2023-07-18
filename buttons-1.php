0746865e3b27030c19d31615b1bed09c24124309
Pkapte <priya_bhimraojikapte_tp@bmc.com>
2023-07-18 18:12:19 +0530

    First Commite

---
+++ buttons-1.php
@@ -0,0 +1,17 @@
+<?php
+	$pageTitle = 'About Us';
+	$bodyClass = 'company page-about-us';
+	include 'php-inc/head.php';
+?>
+    <div class="">
+        <a 
+            class="btn video-play btn-secondary modal-youtube-video-player btn-play-icon"
+            href="/content/bmc/videos.html?vID=pG8wb2ig2Z8" 
+            target="_self">
+            See how Michigan leverages automation (2:16)
+        </a>
+    </div>
+    
+
+
+  <?php include 'php-inc/foot.php'; ?>
